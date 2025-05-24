d2c15d9df3b67afbe2f167fc1bc8a2dea6f541ed
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-05-24 22:31:37 +0530

    new spotify-playlists.js

---
+++ spotify-playlists.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.querySelector('.content');
+    const playlistCards = document.querySelectorAll('.playlist-card');
+    let originalContent = content.innerHTML; 
+
+});
